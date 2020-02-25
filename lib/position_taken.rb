e62559f6f5f363e59ecd593d212f34be1f863a3d
Edward Kane <edwarde.kane@gmail.com>
2020-02-25 19:09:56 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 def
-  array[index_number]
 
+  array[index_number]
+  #position_taken? in lib/position_taken.rb returns false if the board has an empty string " " in the position
+      Failure/Error: expect(position_taken?(board, index)).to be(false)
+
+        expected false
+             got nil
 end
